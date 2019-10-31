8ac833c0b600dd4080d2997d2528a2ce3155417f
Atoluwalogo <atoluwalogo@gmail.com>
2019-10-31 15:54:46 +0100

    my push

---
+++ Javascript/index.js
@@ -0,0 +1,15 @@
+// function greet(name, lastName) {
+//     console.log('Hello ' + name + ' ' + lastName);
+// }
+
+// greet('Tolu',
+//     'Adeboye');
+
+
+
+function add5(a) {
+    return a + 5;
+}
+let x = add5(6);
+
+console.log(x);
---
+++ Javascript/index.test.js
@@ -0,0 +1,5 @@
+const add5 = require('./index.js');
+
+test('adds a + 5 to equal 11', () => {
+    expect(add5(a, 5)).toBe(11);
+});
